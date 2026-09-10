f9bd8037751ea7ec6551236b7e6c861d7e76a3c5
Ange Marquise <angemarquisei@gmail.com>
2026-09-10 21:39:19 +0200

    challenge5

--- React-Lessons/src/main.jsx
+++ React-Lessons/src/main.jsx
@@ -1,11 +1,12 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Challenge4 from './static-pages/Challenge4'
+import Challenge5 from './static-pages/Challenge5'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Challenge4 />
+    <Challenge5 />
+    
   </StrictMode>,
 )
---
+++ React-Lessons/src/static-pages/Challenge5.jsx
@@ -0,0 +1,49 @@
+
+/** Challenge: 
+ * Move the `main` element into its own component called "MainContent" 
+ * and render that component inside the Page component.
+ * 
+ * Do the same with the `footer` element, moving it into a new
+ * component called "Footer"
+*/
+
+function Header() {
+    return (
+        <header>
+            <img src="react-logo.png" width="40px" alt="React logo" />
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+export default Page
+export { Header, MainContent, Footer }
