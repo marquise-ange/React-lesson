5e9617b23b2f46ea5c8a2573fb30da1b1ffaac1b
Ange Marquise <angemarquisei@gmail.com>
2026-09-16 12:27:26 +0200

    adding the image as the background

--- React-Lessons/src/static-pages/Components/Main.jsx
+++ React-Lessons/src/static-pages/Components/Main.jsx
@@ -11,11 +11,11 @@ function Main() {
         <main>
             <h1>Fun Facts about React</h1>
             <ul className="facts-list">
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>Was first released in 2013.</li>
+                <li>Was originally created by Jordan Walke.</li>
+                <li>Has well over 100K stars on GitHub.</li>
+                <li>Is maintained by Meta.</li>
+                <li>Powers thousands of enterprise apps, including mobile apps.</li>
             </ul>
         </main>
     )
