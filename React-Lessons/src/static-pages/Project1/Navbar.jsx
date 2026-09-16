dab0fb22bb2837d1983f920b77c911a31254fa8f
Ange Marquise <angemarquisei@gmail.com>
2026-09-16 18:09:25 +0200

    Project content

--- React-Lessons/src/static-pages/Project1/Navbar.jsx
+++ React-Lessons/src/static-pages/Project1/Navbar.jsx
@@ -1,13 +1,13 @@
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <nav className="navbar">
-            <img src="" alt="logo" className="nav-logo" />
+            <img src="/public/Marquise-logo.jpeg" alt="logo" className="nav-logo" />
             <h1>Ange Marquise IREBE</h1>
             <h2>Front End Developer</h2>
             <h3>Angemarquise.website</h3>
-             <button className="nav-button">Email</button>
-             <button className="nav-button">LinkedIn</button>
+            <button className="nav-email">Email</button>
+            <button className="nav-button">LinkedIn</button>
         </nav>
-    ) 
+    )
 }
 export default Navbar
